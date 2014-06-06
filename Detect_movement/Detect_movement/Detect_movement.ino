60f72f44de96b0d9117e9ee149feb521167789d2
Camille Appert <camille@appert.me>
2014-06-06 22:45:47 +0200

    first session

---
+++ Detect_movement/Detect_movement/Detect_movement.ino
@@ -0,0 +1,62 @@
+/*
+  Button
+
+ Turns on and off a light emitting diode(LED) connected to digital
+ pin 13, when pressing a pushbutton attached to pin 2.
+
+
+ The circuit:
+ * LED attached from pin 13 to ground
+ * pushbutton attached to pin 2 from +5V
+ * 10K resistor attached to pin 2 from ground
+
+ * Note: on most Arduinos there is already an LED on the board
+ attached to pin 13.
+
+
+ created 2005
+ by DojoDave <http://www.0j0.org>
+ modified 30 Aug 2011
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/Button
+ */
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x24, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+// constants won't change. They're used here to
+// set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+bool isFound = false;
+
+void setup() {
+  lcd.init();                      // initialize the lcd
+  lcd.backlight();
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    if (!isFound) {
+      lcd.print("TROUVE!!!!");
+      isFound = true;
+    }
+  }
+  else {
+    lcd.clear();
+  }
+}
